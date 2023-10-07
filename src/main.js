64d93382c5451b5f1118829156e7b95b84d1a2e1
JoseManuel <josemanuelescolano1@gmail.com>
2023-10-07 09:27:51 +0200

    PRUEBAS JAVASCRIT

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
 import "./style.css";
 
 console.log("Hello from main");
+
+/*
+Modulo 4 Funciones
+Funciones: Una función matemática es una regla que asigna a cada número de un conjunto llamado "dominio" un único
+número de otro conjunto llamado "contradominio". En otras palabras, una función toma un valor de entrada (el
+dominio) y produce un valor de salida (el contradominio) de acuerdo con una regla específica
+*/
+
+function doble (x) {
+    return x * 2;
+}
+
+console.log(doble(2));
+console.log(doble(4));
